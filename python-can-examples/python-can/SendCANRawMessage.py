a72f71fca4c6381aee00f3c87673383a6cce5e88
Annamalai C <annamalai@ancitconsulting.com>
2020-03-30 18:54:39 +0530

    Update SendCANRawMessage.py

--- python-can-examples/python-can/SendCANRawMessage.py
+++ python-can-examples/python-can/SendCANRawMessage.py
@@ -5,7 +5,7 @@ Created on 24-Mar-2020
 '''
 import can
 bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
-msg = can.Message(arbitration_id=0xc0ffee,data=[0, 25, 0, 1, 3, 1, 4, 1],is_extended_id=False)
+msg = can.Message(arbitration_id=0x021,data=[0, 25, 0, 1, 3, 1, 4, 1],is_extended_id=False)
 try:
     bus.send(msg)
     print("Message sent on {}".format(bus.channel_info))
