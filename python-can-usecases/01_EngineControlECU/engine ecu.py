c7da012a052b1ba2e8c6e38734fc2f92fe17277e
Kishore <kishoreub1328@gmail.com>
2020-04-17 12:26:59 +0530

    updated on 17/4/20

--- python-can-usecases/01_EngineControlECU/engine ecu.py
+++ python-can-usecases/01_EngineControlECU/engine ecu.py
@@ -10,9 +10,9 @@ can_bus = can.interface.Bus(bustype='socketcan',channel='vcan0',bitrate=1000000)
 def on_Message():
 	while True:
 		response = can_bus.recv()
-		if response.arbitration_id == 0x02:  
+		if response.data == : [1,1,1,1,1,1,1,1] 
 			# Message ID : 0x03 to initialize accelerator and break
-			message = can.Message(arbitration_id=0x03, data=[2,2,2,2,2,2,2,2], is_extended_id=False)
+			message = can.Message(arbitration_id=0x10, data=[2,2,2,2,2,2,2,2], is_extended_id=False)
 			print("Accelerator and Break are initilised")
 			try:
 				can_bus.send(message)
@@ -20,14 +20,10 @@ def on_Message():
 			except can.CanError:
 				print("Message NOT sent")
 
-def on_press(key):
+def initialization():
     
-    print("Key Event Identified")
-    if key.char == 'a':
-        # handles if key press is a to ON the engine
-            
-	  # Message ID : 0x02  
-            message = can.Message(arbitration_id=0x02, data=[1,1,1,1,1,1,1,1], is_extended_id=False)
+   
+            message = can.Message(arbitration_id=0x10, data=[1,1,1,1,1,1,1,1], is_extended_id=False)
             print("Engine is ON")
             try:
                 task = can_bus.send_periodic(message,1.0) # cycle time
@@ -36,12 +32,12 @@ def on_press(key):
                 print("Message NOT sent")
 
 
-def on_Key():
-	keyboard.Listener(on_press=on_press).start()
+
 
 def sendMessage():
 			# Message ID : 0x10
 			message = can.Message(arbitration_id=0x10, data=[0,0,0,0,0,0,0,0], is_extended_id=False)
+			print("Engine is off"")
 			try:
 				can_bus.send(message)
 				print(" 0x10 Message sent on {}".format(can_bus.channel_info))
@@ -51,5 +47,6 @@ def sendMessage():
 if __name__ == '__main__':
 
 	sendMessage()
-	on_Key()
+	initialization()
+	
 	threading.Thread(on_Message()).start()
