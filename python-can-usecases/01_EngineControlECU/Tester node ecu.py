12e5035d0071cfe93b3cc2b5f9e2a4b23bd0b983
Kishore <kishoreub1328@gmail.com>
2020-04-18 12:27:59 +0530

    update on engine ecu and tester node

--- python-can-usecases/01_EngineControlECU/Tester node ecu.py
+++ python-can-usecases/01_EngineControlECU/Tester node ecu.py
@@ -8,8 +8,8 @@ can_bus = can.interface.Bus(bustype='socketcan',channel='vcan0',bitrate=250000)
 
 def _accelerate():
    
-    message = can.Message(arbitration_id= 0x20, data=[10,0,0,0], is_extended_id=False)
-    print("Engine speed accelerated to 10kmph")
+    message = can.Message(arbitration_id= 0x20, data=[10], is_extended_id=False)
+    print("Accelerated to 10kmph")
     try:
         can_bus.send(message)
         #print(" 0x20 Message sent on {}".format(can_bus.channel_info))
@@ -18,8 +18,8 @@ def _accelerate():
 
 def _accelerate2():
    
-    message = can.Message(arbitration_id= 0x20, data=[20,0,0,0], is_extended_id=False)
-    print("Engine speed accelerated to 20kmph")
+    message = can.Message(arbitration_id= 0x20, data=[20], is_extended_id=False)
+    print("Accelerated to 20kmph")
     try:
         can_bus.send(message)
         #print(" 0x20 Message sent on {}".format(can_bus.channel_info))
@@ -28,8 +28,8 @@ def _accelerate2():
  
 def _break():
    
-    message = can.Message(arbitration_id= 0x30, data=[0,0,0,0], is_extended_id=False)
-    print(" Break is applied and Engine speed is 0kmph")
+    message = can.Message(arbitration_id= 0x30, data=[0], is_extended_id=False)
+    print("Break Applied")
     try:
         can_bus.send(message)
         #print(" 0x30 Message sent on {}".format(can_bus.channel_info))
@@ -50,13 +50,41 @@ def on_press(key):
     except AttributeError:
         print(" Unknown Key Event")
         
+def on_Message():
+    #Initiating Vehicle Condition to Default
+    flagAcc1 = True
+    flagAcc2 = True
+    flagBrk = True
+    
+    while True:
+        response = can_bus.recv()
+        data = response.data
+        if response.arbitration_id == 0x010:   
+            if (response.data == bytearray(b'\n\x01\x00\x00')) and flagAcc1: 
+                print("Engine Speed Updated: 10")
+                flagAcc1 = False
+                flagAcc2 = True
+                flagBrk = True
+            if (response.data == bytearray(b'\x14\x01\x00\x00')) and flagAcc2:
+                print("Engine Speed Updated: 20")
+                flagAcc1 = True
+                flagAcc2 = False
+                flagBrk = True
+            if (response.data == bytearray(b'\x00\x01\x00\x00')) and flagBrk:
+                print("Engine Speed Updated: 0")
+                flagAcc1 = True
+                flagAcc2 = True
+                flagBrk = False
+        
 def on_Key():
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
+#     with keyboard.Listener(on_press=on_press) as listener:
+#         listener.join()
+    keyboard.Listener(on_press=on_press).start()
 
 if __name__ == '__main__':
-    
+    #instruction()
     on_Key()
+    threading.Thread(on_Message()).start()
     
             
            
--- python-can-usecases/01_EngineControlECU/engine ecu.py
+++ python-can-usecases/01_EngineControlECU/engine ecu.py
@@ -9,48 +9,39 @@ can_bus = can.interface.Bus(bustype='socketcan',channel='vcan0',bitrate=1000000)
 
 def engine_on():
 	
-    message = can.Message(arbitration_id=0x10, data=[0,0,0,1], is_extended_id=False)
-    print("Engine is ON")
+    message = can.Message(arbitration_id=0x10, data=[0,1,0,0], is_extended_id=False)
     try:
-        can_bus.send(message)
-        #initialize() # cycle time
-        #print(" 0x10 Message sent on {}".format(can_bus.channel_info))
+        periodicTask.modify_data(message)
+        print("Engine is ON")
     except can.CanError:
         print("Message NOT sent")
 
-def engine_off():
-			# Message ID : 0x10
-	message = can.Message(arbitration_id=0x10, data=[0,0,0,0], is_extended_id=False)
-	print("Engine is off")
-	try:
-		can_bus.send(message)
-		#print(" 0x10 Message sent on {}".format(can_bus.channel_info))
-	except can.CanError:
-		print("Message NOT sent")
-
 def apply_break():
-	message = can.Message(arbitration_id=0x10, data=[0,0,0,1], is_extended_id=False)
-	print("Break Applied")
+	message = can.Message(arbitration_id=0x10, data=[0,1,0,0], is_extended_id=False)
 	try:
-		can_bus.send(message)
+		#can_bus.send(message)
+		periodicTask.modify_data(message)
+		print("Break Applied")
 		#print(" 0x10 Message sent on {}".format(can_bus.channel_info))
 	except can.CanError:
 		print("Message NOT sent")
 		
-def apply_accelerate1():
-	message = can.Message(arbitration_id=0x10, data=[0,0,10,1], is_extended_id=False)
-	print("Accileration : 10")
+def apply_accilerate1():
+	message = can.Message(arbitration_id=0x10, data=[10,1,0,0], is_extended_id=False)
 	try:
-		can_bus.send(message)
+		#can_bus.send(message)
+		periodicTask.modify_data(message)
+		print("Accileration : 10")
 		#print(" 0x10 Message sent on {}".format(can_bus.channel_info))
 	except can.CanError:
 		print("Message NOT sent")	
 
-def apply_accelerate2():
-	message = can.Message(arbitration_id=0x10, data=[0,0,20,1], is_extended_id=False)
-	print("Accileration : 20")
+def apply_accilerate2():
+	message = can.Message(arbitration_id=0x10, data=[20,1,0,0], is_extended_id=False)
 	try:
-		can_bus.send(message)
+		#can_bus.send(message)
+		periodicTask.modify_data(message)
+		print("Accileration : 20")
 		#print(" 0x10 Message sent on {}".format(can_bus.channel_info))
 	except can.CanError:
 		print("Message NOT sent")	
@@ -61,18 +52,31 @@ def on_Message():
 		response = can_bus.recv()
 		data = response.data
 		if response.arbitration_id == 0x020:	
-			if response.data == bytearray(b'\n\x00\x00\x00'):
-				apply_accelerate1()
-			if response.data == bytearray(b'\x14\x00\x00\x00'):
-				apply_accelerate2()
+			if response.data == bytearray(b'\n'):
+				apply_accilerate1()
+			if response.data == bytearray(b'\x14'):
+				apply_accilerate2()
 	
 		if response.arbitration_id == 0x030:
 			apply_break()
+			
+def intiateVehicle():
+	message = can.Message(arbitration_id=0x10, data=[0,0,0,0], is_extended_id=False)
+	try:
+		task = can_bus.send_periodic(message, 1.0)
+		print("Vehicle Initiated")
+		return task
+		#print(" 0x10 Message sent on {}".format(can_bus.channel_info))
+	except can.CanError:
+		print("Message NOT sent")
 
 if __name__ == '__main__':
-
-	engine_off()
+	
+	periodicTask = intiateVehicle()
 	engine_on()
 	threading.Thread(on_Message()).start()
 	
 	#threading.Thread(on_Message()).start()
+	
+	
+	
